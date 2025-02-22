c5f56d5089b4a8b608d5a14dc37259906416cde6
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-02-22 01:01:06 -0800

    Separate indexRouter get logic

--- routes/indexRouter.ts
+++ routes/indexRouter.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from "express";
+import { genresGet } from "../controllers/indexController.js";
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req: Request, res: Response) => res.render("index"));
+indexRouter.get("/", genresGet);
 
 export default indexRouter;
