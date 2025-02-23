e4253f881b89464b2fcf849702cad686b9fab494
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-02-22 23:34:02 -0800

    Create 3/4 genres queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,20 +1,17 @@
 import pool from "./pool.js";
 
-/*
-export async function getMessageById(msgIndex: number) {
-  const { rows } = await pool.query("SELECT * FROM messages WHERE id = $1", [
-    msgIndex,
-  ]);
-
-  return rows[0];
-}
-
-*/
-
 export async function getGenres() {
   const { rows } = await pool.query("SELECT * FROM genres");
 
   return rows;
+}
+
+export async function getGenreById(genreId: number) {
+  const { rows } = await pool.query("SELECT * FROM genres WHERE id = $1", [
+    genreId,
+  ]);
+
+  return rows[0];
 }
 
 export async function getArtists() {
@@ -23,10 +20,28 @@ export async function getArtists() {
   return rows;
 }
 
+export async function getArtistsByGenre(genreId: number) {
+  const { rows } = await pool.query(
+    "SELECT artists.name, artists.image, artists.id FROM artists JOIN artist_genres ON (artists.id=artist_genres.artist_id) JOIN genres ON (genres.id=artist_genres.genre_id) WHERE genres.id = $1",
+    [genreId]
+  );
+
+  return rows;
+}
+
 export async function getAlbums() {
   const { rows } = await pool.query("SELECT * FROM albums");
 
   return rows;
+}
+
+export async function getAlbumArtist(albumName: string) {
+  const { rows } = await pool.query(
+    "SELECT name FROM artists JOIN album_artists ON (artists.id=album_artists.artist_id) JOIN albums ON (albums.id=album_artists.album_id) WHERE album = $1;",
+    [albumName]
+  );
+
+  return rows[0];
 }
 
 /*
