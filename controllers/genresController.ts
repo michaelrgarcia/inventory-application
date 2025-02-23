665d2d0a9375925c1f6baa2effe91f89c8127bf9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-02-22 19:05:17 -0800

    Scaffold genre add route

--- controllers/genresController.ts
+++ controllers/genresController.ts
@@ -1,9 +1,44 @@
 import { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 export function genresGet(req: Request, res: Response) {
   res.render("genres");
 }
 
 export function addGenreGet(req: Request, res: Response) {
-  res.render("add-genre");
+  res.render("addGenre");
 }
+
+export const validateGenre = [
+  body("genreName")
+    .trim()
+    .escape()
+    .isLength({ min: 1, max: 50 })
+    .withMessage(`Genre name cannot be longer than 50 characters.`),
+  body("genreDescription")
+    .trim()
+    .escape()
+    .isLength({ min: 1, max: 255 })
+    .withMessage(`Genre description cannot be longer than 255 characters.`),
+];
+
+export const addGenrePost = [
+  validateGenre,
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("addGenre", {
+        errors: errors.array(),
+      });
+    }
+
+    const { genreName, genreDescription } = req.body;
+
+    console.log(genreName, genreDescription);
+
+    // insert genre query
+
+    res.status(200).redirect("/genres");
+  },
+];
--- routes/genresRouter.ts
+++ routes/genresRouter.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { addGenreGet, genresGet } from "../controllers/genresController.js";
+import {
+  addGenreGet,
+  addGenrePost,
+  genresGet,
+} from "../controllers/genresController.js";
 
 const genresRouter = Router();
 
 genresRouter.get("/", genresGet);
+
 genresRouter.get("/new", addGenreGet);
+// "as any" due to error in express types module
+genresRouter.post("/new", addGenrePost as any);
 
 export default genresRouter;
