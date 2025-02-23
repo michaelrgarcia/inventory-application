4de70282e1f47ef73131fa1eab8d223f51aa5f8b
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-02-23 01:22:59 -0800

    Finish addArtist and addGenre

--- db/queries.ts
+++ db/queries.ts
@@ -1,5 +1,12 @@
 import pool from "./pool.js";
 
+export async function addGenre(name: string, description: string) {
+  await pool.query("INSERT INTO genres (name, description) VALUES ($1, $2)", [
+    name,
+    description,
+  ]);
+}
+
 export async function getGenres() {
   const { rows } = await pool.query("SELECT * FROM genres");
 
@@ -12,6 +19,23 @@ export async function getGenreById(genreId: number) {
   ]);
 
   return rows[0];
+}
+
+export async function addArtist(
+  name: string,
+  description: string,
+  imgUrl: string,
+  genre: string
+) {
+  await pool.query(
+    "INSERT INTO artists (name, description, image) VALUES ($1, $2, $3)",
+    [name, description, imgUrl]
+  );
+
+  await pool.query(
+    "INSERT INTO artist_genres (artist_id, genre_id) SELECT artists.id, genres.id FROM artists, genres WHERE artists.name = $1 AND genres.name = $2",
+    [name, genre]
+  );
 }
 
 export async function getArtists() {
@@ -54,7 +78,7 @@ export async function getAlbums() {
 
 export async function getAlbumsByGenre(genreId: number) {
   const { rows } = await pool.query(
-    "SELECT albums.album, albums.cover, albums.id FROM albums JOIN album_genres ON (albums.id=album_genres.album_id) JOIN genres ON (genres.id=album_genres.genre_id) WHERE genres.id = $1",
+    "SELECT albums.album, albums.cover, albums.id, albums.yr FROM albums JOIN album_genres ON (albums.id=album_genres.album_id) JOIN genres ON (genres.id=album_genres.genre_id) WHERE genres.id = $1",
     [genreId]
   );
 
@@ -86,16 +110,3 @@ export async function getAlbumArtist(albumId: number) {
 
   return rows[0];
 }
-
-/*
-export async function insertMessage(
-  username: string,
-  message: string,
-  dateAdded: Date
-) {
-  await pool.query(
-    "INSERT INTO messages (username, text, added) VALUES ($1, $2, $3)",
-    [username, message, dateAdded.toISOString()]
-  );
-}
-*/
